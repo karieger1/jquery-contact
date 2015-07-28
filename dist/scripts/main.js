c6ae7cb471e5e4277a4bedb65b17884b5640e06a
Kristen Rieger <karieger1@gmail.com>
2015-07-28 00:18:55 -0500

    done

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,24 +1,110 @@
 $(document).on('ready', function(){
 
-
+var $main = $("#main");
 var $button = $("#button");
 var $form = $("#form");
 var $name = $("#name");
 var $email = $("#email");
 var $website = $("#website");
 var $message = $("#message");
+var $nameerr = $("#nameerror");
+var $emailerr = $("#emailerror");
+var $websiteerr = $("#websiteerror");
+var $commenterr = $("#commenterror");
 
-$button.click(submitForm);
+errormessage = [$nameerr, $emailerr, $websiteerr, $commenterr];
+input = [$name, $email, $website, $message];
 
-function submitForm(e) {
-	e.preventDefault();
-	if (validate()==true) {
-		var success = "Thank you for bothering us," + $name.val() +" . We'll begin spamming you very soon."
+
+$button.click(checkErrors);
+
+function checkErrors(e) {
+	noErrors();
+
+	for(var i=0; i<input.length; i++) {
+
+		if(validator.isNull(input[i].val())){
+			e.preventDefault();
+			errormessage[i].show();
+			input[i].css("2px solid pink");
+		}
+	}
+	
+	if(checkEmail(e) && checkwebsite(e)) {
+
+		if(validator.isNull($name) && validator.isNull($message)) {
+			$nameerr.show();
+			$commenterr.show();
+			e.preventDefault();
+		}
+
+		else if(validator.isNull($name)) {
+			$nameerr.show();
+			e.preventDefault();
+		}
+
+		else if(validator.isNull($message)) {
+			$commenterr.show();
+			e.preventDefault();
+		}
+		else {
+			successMessage(e);
+		}
+	}
+		else {
+			e.preventDefault();
+		}
+}
+
+function checkEmail(e){
+
+	if(!validator.isEmail($email.val())){
+		
+		$emailerr.show();
+		$email.css("2px solid pink");
+		e.preventDefault();
+	
+	}
+	else{
+
+		return true
 	}
 }
 
+function checkwebsite(e){
 
-$button.mouseover(function () {
+	if(!validator.isURL($website.val())){
+		
+		$websiteerr.show();
+		$website.css("2px solid pink");
+		e.preventDefault();
+	}
+	else {
+		
+		return true
+	}
+}
+
+function successMessage(e){
+
+	e.preventDefault();
+	$main.hide();
+	$('#successpage').html('<p>Thanks so much for contacting us, butthole. You will never receive a reply!</p>');
+	$('#successpage').show();
+
+	
+}
+
+function noErrors () {
+
+	for(var i =0; i <errormessage.length; i++){
+
+		errormessage[i].hide();
+		input[i].css("2px solid pink");
+	}
+}
+
+	$button.mouseover(function () {
 		$button.css('transition', 'background-color 0.5s ease');
 		$button.css('background-color', '#88C470');
 		
